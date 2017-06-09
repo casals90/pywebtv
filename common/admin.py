f45df5de9b6dc4c06d46a87554c7ad5b1b24f040
jcasals <jcasals@gaa9.com>
2017-06-09 13:40:14 +0200

    adding jquery

--- common/admin.py
+++ common/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from common.models import user_model
 
 
-admin.site.register(user_model.User)
+@admin.register(user_model.User)
+class UserAdmin(admin.ModelAdmin):
+    model = user_model.User
