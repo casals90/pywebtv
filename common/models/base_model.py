6ab126609c79e40920d0fd8b571e237264211b06
jcasals <jcasals@gaa9.com>
2017-06-09 10:03:03 +0200

    create base model

--- common/models/base_model.py
+++ common/models/base_model.py
@@ -1,5 +1,30 @@
 # -*- encoding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext as _
 
 
-class BaseModel():
-    pass
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    # creator_by modify_by -> User
+    creation_date = models.DateTimeField(
+        blank=False,
+        null=False,
+        auto_now_add=True,
+        db_index=True,
+        verbose_name=_('Creation date'),
+    )
+    modification_date = models.DateTimeField(
+        blank=False,
+        null=False,
+        auto_now_add=True,
+        db_index=True,
+        verbose_name=_('Modification date'),
+    )
+    delete_date = models.DateTimeField(
+        blank=True,
+        null=True,
+        db_index=True,
+        verbose_name=_('Delete date')
+    )
