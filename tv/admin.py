05b76dc2b11a6a2433f3681d905928e8c0ce8b1d
Jordi Casals <jordicasals@MacBook-MacBook-Pro-de-Jordi.local>
2017-08-01 19:28:56 +0200

    adding model in admin

--- tv/admin.py
+++ tv/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from tv.models import streaming_channel
 
-# Register your models here.
+@admin.register(streaming_channel.StreamingChannel)
+class StreamingChannelAdmin(admin.ModelAdmin):
+    model = streaming_channel.StreamingChannel
